f76d122e86897de78fb7f15f1399f5f87a8156a6
Emelie Svensson <emeliesv@live.se>
2021-01-18 10:51:20 +0100

    added backend-folder and started working on schemas

---
+++ frontend/src/components/BlogPostInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { EditText, EditTextarea } from 'react-edit-text'
+import 'react-edit-text/dist/index.css'
+
+export const BlogPostInput = () => {
+  return (
+    <>
+      <EditText />
+      <EditTextarea />
+    </>
+  )
+}
